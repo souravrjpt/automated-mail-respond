c2543e6adf7adeb7d6a56e0280ade2a01f932120
souravrjpt <souravrajput2760@gmail.com>
2024-06-27 20:49:52 +0530

    Removed the text.js file

--- text.js
+++
@@ -1,6 +0,0 @@
-
-const clientId = "1068170586914-dao0f2ikdrggi509l2a6vfavcjh7uv42.apps.googleusercontent.com";
-const clientSecret = "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";
-const redirectURI = "https://developers.google.com/oauthplayground";
-const refreshToken ="1//04RAluUHUCY6FCgYIARAAGAQSNwF-L9IrqOUs5qrCZkAQbzSRBnnyeTSymgwEeVO5fJfkjwa-82paElX6FKcRam_pudwX6EvihkI";
-module.exports = { clientId, clientSecret, redirectURI, refreshToken };
